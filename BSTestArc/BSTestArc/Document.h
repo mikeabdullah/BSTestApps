8dbe9a98cec0e3f6f928fcb0eb771c227d45d6de
Abizer Nasir <abizern@abizern.org>
2012-12-19 09:44:14 +0000

    Initial import

---
+++ BSTestArc/BSTestArc/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  BSTestArc
+//
+//  Created by Abizer Nasir on 19/12/2012.
+//  Copyright (c) 2012 Jungle Candy Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
